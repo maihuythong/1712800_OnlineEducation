7fcdfe5a1c3c74be276e485c1cc763a310803506
Mai Huy Thong <1712800@student.hcmus.edu.vn>
2021-01-21 19:33:10 +0700

    format appNav, test deploy

--- src/navigation/AppNav/index.js
+++ src/navigation/AppNav/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeStackScreen from './HomeStackScreen';
-import BrowseStackScreen from './BrowseStackScreen';
-import SearchStackScreen from './SearchStackScreen';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import CategoryStackScreen from './CategoryStackScreen';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeStackScreen from "./HomeStackScreen";
+import BrowseStackScreen from "./BrowseStackScreen";
+import SearchStackScreen from "./SearchStackScreen";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import CategoryStackScreen from "./CategoryStackScreen";
+import { useTranslation } from "react-i18next";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
-  const { t } = useTranslation('tab_navigator', { useSuspense: false });
+  const { t } = useTranslation("tab_navigator", { useSuspense: false });
 
   return (
     <Tab.Navigator
       tabBarOptions={{
-        activeTintColor: '#0c71ac',
-        inactiveTintColor: '#d9d9d9',
+        activeTintColor: "#0c71ac",
+        inactiveTintColor: "#d9d9d9",
         style: {
-          backgroundColor: '#1f242a',
+          backgroundColor: "#1f242a",
         },
         labelStyle: {
           fontSize: 16,
@@ -31,48 +31,48 @@ const AppNavigator = () => {
       }}
     >
       <Tab.Screen
-        name={t('home')}
+        name={t("home")}
         component={HomeStackScreen}
         options={{
           // title: 'Home',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name='home' color={'#009dc4'} size={30} />
+            <MaterialCommunityIcons name="home" color={"#009dc4"} size={30} />
           ),
         }}
       />
       <Tab.Screen
-        name={t('download')}
+        name={t("download")}
         component={CategoryStackScreen}
         options={{
-          title: t('download'),
+          title: t("download"),
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons
-              name='certificate'
-              color={'#009dc4'}
+              name="certificate"
+              color={"#009dc4"}
               size={30}
             />
           ),
         }}
       />
       <Tab.Screen
-        name={t('browse')}
+        name={t("browse")}
         component={BrowseStackScreen}
         options={{
-          title: t('browse'),
+          title: t("browse"),
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name='folder' color={'#009dc4'} size={30} />
+            <MaterialCommunityIcons name="folder" color={"#009dc4"} size={30} />
           ),
         }}
       />
       <Tab.Screen
-        name={t('search')}
+        name={t("search")}
         component={SearchStackScreen}
         options={{
-          title: t('search'),
+          title: t("search"),
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons
-              name='magnify'
-              color={'#009dc4'}
+              name="magnify"
+              color={"#009dc4"}
               size={30}
             />
           ),
